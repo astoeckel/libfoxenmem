e68f7410f080234074923af14804461fa990db05
Andreas Stöckel <astoecke@uwaterloo.ca>
2018-07-14 03:08:35 -0400

    Add fx_zero_aligned_mem()

--- foxen/mem.h
+++ foxen/mem.h
@@ -28,6 +28,7 @@
 #ifndef FOXEN_MEM_H
 #define FOXEN_MEM_H
 
+#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -109,5 +110,34 @@ static inline void *fx_mem_align(void **mem, uint32_t size) {
 	*mem = (void *)((uintptr_t)res + size);
 	return res;
 }
+
+/**
+ * Fills the given memory region with zeros. In contrast to memset(mem, 0, size)
+ * this assumes that the pointer is aligned at the FX_ALIGN boundary and that
+ * we can write multiples of FX_ALIGN bytes at once. This is potentially
+ * dangerous, so do not use this function if you don't know exactly what you're
+ * doing.
+ *
+ * @param mem is a pointer at the memory region that should be zeroed out. This
+ * pointer is assumed to be aligned.
+ * @param size is the size of the memory region that should be zeroed in bytes.
+ * This value is effectively rounded up to a multiple of FX_ALIGN
+ */
+static inline void fx_zero_aligned_mem(void *mem, uint32_t size) {
+	assert((((uintptr_t)mem) & (FX_ALIGN - 1)) == 0); /* mem must be aligned */
+	mem = FX_ASSUME_ALIGNED(mem);
+	for (uint32_t i = 0; i < (size + FX_ALIGN - 1) / FX_ALIGN; i++) {
+		((uint64_t*)mem)[2 * i + 0] = 0; /* If we're lucky, this loop is */
+		((uint64_t*)mem)[2 * i + 1] = 0; /* unrolled and vectorised. */
+	}
+}
+
+/**
+ * Macro that fills the structure pointed at by P with zeros. See
+ * fx_zero_aligned_mem() regarding potential dangers.
+ */
+#define FX_ZERO_ALIGNED_MEM(P) do { \
+	fx_mem_zero(P, sizeof(*(P))); \
+} while(0)
 
 #endif /* FOXEN_MEM_H */
