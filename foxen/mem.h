1970471f06d2f8077a382871e65978678bb29ff6
Andreas Stöckel <astoecke@uwaterloo.ca>
2018-09-02 11:18:22 -0400

    Autoformat

--- foxen/mem.h
+++ foxen/mem.h
@@ -127,8 +127,8 @@ static inline void fx_mem_zero_aligned(void *mem, uint32_t size) {
 	assert((((uintptr_t)mem) & (FX_ALIGN - 1)) == 0); /* mem must be aligned */
 	mem = FX_ASSUME_ALIGNED(mem);
 	for (uint32_t i = 0; i < (size + FX_ALIGN - 1) / FX_ALIGN; i++) {
-		((uint64_t*)mem)[2 * i + 0] = 0; /* If we're lucky, this loop is */
-		((uint64_t*)mem)[2 * i + 1] = 0; /* unrolled and vectorised. */
+		((uint64_t *)mem)[2 * i + 0] = 0; /* If we're lucky, this loop is */
+		((uint64_t *)mem)[2 * i + 1] = 0; /* unrolled and vectorised. */
 	}
 }
 
@@ -136,8 +136,10 @@ static inline void fx_mem_zero_aligned(void *mem, uint32_t size) {
  * Macro that fills the structure pointed at by P with zeros. See
  * fx_mem_zero_aligned() regarding potential dangers.
  */
-#define FX_MEM_ZERO_ALIGNED(P) do { \
-	fx_mem_zero_aligned(P, sizeof(*(P))); \
-} while(0)
+#define FX_MEM_ZERO_ALIGNED(P)                \
+	do {                                      \
+		fx_mem_zero_aligned(P, sizeof(*(P))); \
+	} while (0)
+
 
 #endif /* FOXEN_MEM_H */
